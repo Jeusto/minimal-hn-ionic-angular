86ed8cefc9b4a772d232a3173279aabd1a334dba
Arhun Saday <arhunsad@gmail.com>
2023-05-30 20:28:30 +0200

    Move profile and search page to "pages" subfolder

--- src/app/pages/profile/profile.module.ts
+++ src/app/pages/profile/profile.module.ts
@@ -3,18 +3,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ProfilePage } from './profile.page';
-import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
-
 import { ProfilePageRoutingModule } from './profile-routing.module';
 
 @NgModule({
-  imports: [
-    IonicModule,
-    CommonModule,
-    FormsModule,
-    ExploreContainerComponentModule,
-    ProfilePageRoutingModule,
-  ],
+  imports: [IonicModule, CommonModule, FormsModule, ProfilePageRoutingModule],
   declarations: [ProfilePage],
 })
 export class ProfilePageModule {}
