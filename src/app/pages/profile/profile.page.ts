6abb08e4ae176e95692b83b3381a2b2fd51051d3
Arhun Saday <arhunsad@gmail.com>
2023-05-31 21:58:35 +0200

    Remove empty scss and test files

--- src/app/pages/profile/profile.page.ts
+++ src/app/pages/profile/profile.page.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-profile',
   templateUrl: 'profile.page.html',
-  styleUrls: ['profile.page.scss'],
 })
 export class ProfilePage {
   constructor() {}
--- src/app/pages/search/search.page.ts
+++ src/app/pages/search/search.page.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-search',
   templateUrl: 'search.page.html',
-  styleUrls: ['search.page.scss'],
 })
 export class SearchPage {
   constructor() {}
