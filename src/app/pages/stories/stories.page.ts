bf824aae67dceb01845627013446f8520a250463
Arhun Saday <arhunsad@gmail.com>
2023-06-03 13:43:12 +0200

    Add scroll to top button

--- src/app/pages/stories/stories.page.ts
+++ src/app/pages/stories/stories.page.ts
@@ -1,20 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { loadStories } from 'src/app/stores/stories/stories.actions';
 import { AppState } from 'src/app/stores/stories/stories.models';
 import { selectMainPageStories } from 'src/app/stores/stories/stories.selectors';
 import { BrowserService } from 'src/app/services/browser.service';
+import { IonContent } from '@ionic/angular';
 
 @Component({
   selector: 'app-stories',
   templateUrl: 'stories.page.html',
+  styleUrls: ['stories.page.scss'],
 })
 export class StoriesPage implements OnInit {
+  @ViewChild(IonContent) content!: IonContent;
   mainPageStories$: Observable<Partial<AppState['stories']>> = new Observable();
   currentPage: number = 0;
   totalPages: number = 0;
   canLoadMoreStories = true;
+  showScrollTopButton = false;
+  previousScrollPosition = 0;
 
   constructor(
     private store: Store<AppState>,
@@ -47,5 +52,22 @@ export class StoriesPage implements OnInit {
   handleRefresh(event: any) {
     this.store.dispatch(loadStories({ page: 0 }));
     event.target.complete();
+  }
+
+  onScroll(event: any) {
+    const scrollTop = event.detail.scrollTop;
+    const isScrollingUp = scrollTop < this.previousScrollPosition;
+
+    if (isScrollingUp && scrollTop > 100) {
+      this.showScrollTopButton = true;
+    } else {
+      this.showScrollTopButton = false;
+    }
+
+    this.previousScrollPosition = scrollTop;
+  }
+
+  scrollToTop() {
+    this.content.scrollToTop();
   }
 }
