6fe8d134b15d0877637c2222d90566dc3341891e
Arhun Saday <arhunsad@gmail.com>
2023-06-07 23:20:18 +0200

    Update app id

--- android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -1,4 +1,4 @@
-package io.ionic.starter;
+package com.jeusto.minimalhn;
 
 import com.getcapacitor.BridgeActivity;
 
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.jeusto.minimalhn',
   appName: 'Minimal HN',
   webDir: 'www',
   server: {
